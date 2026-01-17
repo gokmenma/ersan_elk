1b53d624010f2c42277e50759c7ffe0938948486
magokmen <ertanguness@gmail.com>
2026-01-17 21:58:39 +0300

    deploy

---
+++ deploy.php
@@ -0,0 +1,16 @@
+<?php
+$secret = 'a9f3cE7QmL2R6XKZ8N4VwH0JYbP5DStU';
+
+$signature = 'sha256=' . hash_hmac(
+    'sha256',
+    file_get_contents('php://input'),
+    $secret
+);
+
+if (!hash_equals($signature, $_SERVER['HTTP_X_HUB_SIGNATURE_256'] ?? '')) {
+    http_response_code(403);
+    exit('Yetkisiz');
+}
+
+$output = shell_exec('cd /home/mbeyazil/repositories/ersan_elk && git pull 2>&1');
+echo "<pre>$output</pre>";
