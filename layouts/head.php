4a7c0f1a4e7a4a4a2fa1e957c19ed7bd03ac6439
magokmen <ertanguness@gmail.com>
2026-01-24 20:28:39 +0300

    head düzenleme

--- layouts/head.php
+++ layouts/head.php
@@ -1,8 +1,7 @@
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta content="Can Sağlık Sendikası" name="description" />
-<meta content="Themesdesign" name="author" />
+<meta content="Ersan Elektrik | Yönetim Paneli" name="description" />
+<meta content="Ersan Elektrik" name="mbeyazilim" />
 <!-- App favicon -->
 <link rel="shortcut icon" href="../../assets/images/fav.jpg">
 <link rel="canonical" href="https:/www.softran.online/index" />
-
